dd9f53a44396c9511c68da9797e8ee61cf109f12
Antoine <baldassarian@gmail.com>
2017-03-06 16:33:52 -0500

    add graph titles
    with html

--- definitions.R
+++ definitions.R
@@ -2,6 +2,7 @@ studies = c("ARIC", "WHI_Whites", "mega", "metal")
 substudies = list(ARIC = c("aric_b", "aric_w"), WHI_Whites = c("GARNET", "GECCO", "HIPFX", "MOPMAP", "WHIMS"), 
                   mega = "", metal = "")
 traits = c("pwav", "pr_seg", "qrs", "st", "twav", "tp")
+traitslab = list(pwav = "P-wave", pr_seg = "PR Segment", qrs = "QRS Interval", st = "ST Segment", twav = "T-wave", tp = "TP Segment")
 prefix = list(ARIC="ARIC_", WHI_Whites = "", mega = "mega_", metal = "")
 fixt = list(mega = "SOL + Share", metal = "Meta-Analysis")
 annolist <- list(multicolor=1, any=2, PR=3,
